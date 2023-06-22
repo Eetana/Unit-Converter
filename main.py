91150212dadbe1a0eaf838e7bebd2645ca21275f
Eetana <64721985+Eetana@users.noreply.github.com>
2023-06-23 01:54:25 +1000

    I was in the wrong directory

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ sw = round(screen_info.width * 0.3)
 sh = round(screen_info.height * 0.5)
 
 
-# Normal; meter, kg, liter, kelvin/celcius
+# SI; meter, kg, liter, kelvin/celcius
 # Length: foot, inch, yard, mile, nautical mile 
 # Mass: ounce, pound, stone, ton
 # Capacity: gallon, pint, quart, fluid ounce
@@ -71,24 +71,31 @@ def fah_to_ran(fah:float):
 window = tk.Tk()
 
 # Set the window title
-window.title("Canvas Example")
+window.title("Unit Converter")
 dim_string = f'{sw}x{sh}'
-
 window.geometry(dim_string)
 
-# # Set the desired canvas dimensions
-# cw = 0.3 * sw
-# ch = 0.3 * sh
+# Create a canvas
+canvas = tk.Canvas(window, width=sw * 0.8, height=sh * 0.8)
+canvas.pack()
+canvas.place(x=sw//2, y=sh//2)
 
-# # Calculate the coordinates to center the canvas
-# x = canvas_width // 2
-# y = canvas_height // 2
+def display_text():
+   global entry
+   string= entry.get()
+   label.configure(text=string)
 
-# # Create a canvas
-# canvas = tk.Canvas(window, width=canvas_width, height=canvas_height)
-# canvas.pack()
-# canvas.place(x=x, y=y)
+#Initialize a Label to display the User Input in the desired output
+label= tk.Label(canvas, text="", font=("Courier 22 bold"))
+label.pack()
 
+#Create an Entry widget to accept User Input
+entry= tk.Entry(canvas, width= 40)
+entry.focus_set()
+entry.pack()
+
+#Create a Button to validate Entry Widget
+tk.Button(canvas, text= "Okay",width= 20, command= display_text).pack(pady=20)
 # canvas.create_rectangle(50, 50, 350, 250, fill="blue")
 # canvas.create_text(200, 150, text="Centered Canvas", fill="white", font=("Arial", 16))
 
