28a8292d1fc20cb5d4fa48587aa74f3e19726efc
Eetana <64721985+Eetana@users.noreply.github.com>
2023-06-24 16:48:10 +1000

    Working on logic for display

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ screen_info = screeninfo.get_monitors()[0]
 sw = round(screen_info.width * 0.3)
 sh = round(screen_info.height * 0.5)
 
+# List of available units that can be selected
+available_units = ['K','C','F','R','L','gal','pt','qt','oz','km','m','mm','inch','ft','yd','mi','NM','kg','t','lb','LT', 'st']
+
 # Initialise variable to store the selected units
 initial_unit = ""
 desired_unit = ""
@@ -59,22 +62,65 @@ def change_text(selection):
 
 # Function to determine the original unit
 def init_unit(selection):
+        
     global initial_unit
+    global desired_unit
     global old_menu
     global new_menu
+    global button
+    
     initial_unit = selection
+    
+    if desired_unit == "": button.configure(state=tk.DISABLED)
+    
+    # Reset list of options
+    for choice in available_units:
+        new_menu['menu'].add_command(label=choice, command=tk._setit(wanted_unit, choice))
+        
+    if selection in ['K','C','F','R']:
+        new_menu['menu'].delete(0, 'end')
+        for choice in ['K','C','F','R']:
+            if selection == choice: continue
+            new_menu['menu'].add_command(label=choice, command=tk._setit(wanted_unit, choice))
+            
+        if desired_unit not in ['K','C','F','R']:
+            wanted_unit.set("-")
+            desired_unit = ""
+        
+    elif selection in ['L','gal','pt','qt','oz']:
+        new_menu['menu'].delete(0, 'end')
+        for choice in ['L','gal','pt','qt','oz']:
+            if selection == choice: continue
+            new_menu['menu'].add_command(label=choice, command=tk._setit(wanted_unit, choice))
+            
+        if desired_unit not in ['L','gal','pt','qt','oz']:
+            wanted_unit.set("-")
+            desired_unit = ""
+    
+    elif selection in ['km','m','mm','inch','ft','yd','mi','NM']:
+        new_menu['menu'].delete(0, 'end')
+        for choice in ['km','m','mm','inch','ft','yd','mi','NM']:
+            if selection == choice: continue
+            new_menu['menu'].add_command(label=choice, command=tk._setit(wanted_unit, choice))    
+            
+        if desired_unit not in ['km','m','mm','inch','ft','yd','mi','NM']:
+            wanted_unit.set("-")
+            desired_unit = ""
+            
+    if initial_unit != desired_unit: button.configure(state=tk.NORMAL)
+            
     
 # Function that triggers for above
 def unit_selection(selection):
+    global initial_unit
     global desired_unit
-    # global old_menu
-    # global new_menu
+    global old_menu
+    global new_menu
+    global button
+    
     desired_unit = selection
-    # if selection in ['K','C','F','R']:
-    #     new
-
-# List of available units that can be selected
-available_units = ['K','C','F','R','L','gal','pt','qt','oz','km','m','mm','inch','ft''yd','mi','NM','kg','t','lb','LT', 'st']
+    
+    if initial_unit != desired_unit: button.configure(state=tk.NORMAL)
 
 # Create the pull down selection for the initial unit
 old_unit = tk.StringVar()
@@ -82,7 +128,7 @@ old_unit.set("-")
 old_menu = tk.OptionMenu(window, old_unit, *available_units, command=init_unit)
 old_menu.place(x=sw*0.32, y=sh*0.548)
 
-# Create the pull down selection for the initial unit
+# Create the pull down selection for the new unit
 wanted_unit = tk.StringVar()
 wanted_unit.set("-")
 new_menu = tk.OptionMenu(window, wanted_unit, *available_units, command=unit_selection)
@@ -92,21 +138,29 @@ new_menu.place(x=sw*0.32, y=sh*0.608)
 def display_text():
     
     global value_entry
+    
     convereted_numver_string = str(value_entry.get())
     string = 'Converted Value: ' + convereted_numver_string
     value_label.configure(text=string)
     
     global initial_unit
     global desired_unit
-    initial_unit = desired_unit 
+    # initial_unit = desired_unit 
+    # desired_unit = ""
+    change_text(initial_unit)
+    print(initial_unit, desired_unit)
     
     old_unit.set(desired_unit)
     wanted_unit.set("-")
-    
-    change_text(desired_unit)
 
-#Create a Button to validate Entry Widget
-tk.Button(canvas, text= "Convert",width=20,command= display_text).place(x=(sw-20)*0.4, y=sh*0.68)
+    for choice in available_units:
+        new_menu['menu'].add_command(label=choice, command=tk._setit(wanted_unit, choice))
+        
+    button.configure(state=tk.DISABLED)
+
+#Create a Button to convert and default to disabled
+button = tk.Button(canvas, text= "Convert",width=20,command= display_text)
+button.place(x=(sw-20)*0.4, y=sh*0.68)
+button.configure(state=tk.DISABLED)
 
 window.mainloop()
-
