d0ef105b5f1d59eb63709ef37eb3e295483cccbd
Eetana <64721985+Eetana@users.noreply.github.com>
2023-06-25 01:01:22 +1000

    Working on lengths

--- lengths.py
+++ lengths.py
@@ -1,1 +1,22 @@
 'km','m','mm','inch','ft''yd','mi','NM'
+# m to km
+def m_to_km(m:float):
+    km = m / 1000
+    return km
+
+# km to m
+def km_to_m(km:float):
+    m = km * 1000
+    return m
+
+# m to mm
+def m_to_mm(m:float):
+    mm = m * 1000
+    return mm
+
+# mm to m
+def mm_to_m(mm:float):
+    m = mm / 1000
+    return m
+
+# mm to inch
